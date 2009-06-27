26a049be7dbd0b2582266b50d7348ad33929e0fe
Yue Peng <yp@localhost.localdomain>
2009-06-27 20:35:09 +0800

    setx/mkdir/bind is ok

--- main.cc
+++ main.cc
@@ -2,7 +2,8 @@
 // #include <fstream>
 // #include <sstream>
 // #include <ostream>
- #include <unistd.h>
+#include <vector>
+#include <unistd.h>
 
 #include <string>
 #include <cstdio>
@@ -18,12 +19,11 @@
 // #include "direct.h"	// _mkdir()
 #include <sys/stat.h>
 #include <sys/types.h>
+
 // #include "conio.h"	// _kbhit() _getch()
 #include <stdio.h>
-
 // #define ___ESC	if ( _kbhit() && ( _getch() == 0x1b ) )	break; 
 
-	/*
 #include "stdarg.h"	// va_list va_start() va_arg() va_end()
 template <typename T, typename S>
 class BIND {
@@ -33,14 +33,13 @@ public:
 		va_list ptr; 
 		va_start(ptr, numb); 
 		while(numb--){
-			S t va_arg(ptr, S); 
+			S t=va_arg(ptr, S); 
 			vt.push_back(static_cast<T>(t)); 
 		}
 		va_end(ptr); 
 		return vt; 
 	}
 }; 
-	*/
 
 using namespace std;
 
@@ -58,13 +57,17 @@ main (int argc, char **argv)
   unsigned int p = pow(2.0,31);
   cout << setfill('0') << setw(30) << p << ": 2^31" << endl;
   cout << UINT_MAX << ": uint_max" << endl;
+
+  vector<string> vs=BIND<string, char*>()(3,"a","bb","ccc");
+  for (int i=0;i<vs.size();++i)
+	  cout << vs[i] << endl;
   
   ::mkdir("newdir",0777);
 
   for (int i=0;i<100;++i) 
   {
 	//___ESC;
-	cout << "y" << getchar() << flush;
+	cout << "y" << flush;
   	sleep(1);
   }
 
